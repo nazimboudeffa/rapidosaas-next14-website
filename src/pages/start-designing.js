4a7d4c4a76fbfbf9aa37b4bd322612df018ac0f9
unicorn <boudeffa@gmail.com>
2024-08-02 23:17:02 +0200

    Removed h1

--- src/pages/start-designing.js
+++ src/pages/start-designing.js
@@ -6,13 +6,9 @@ function Design() {
             <PageMetaTags title="Start Designing" description={"Meta description here"} url="/start-designing"/>
             <div className="grid place-items-center w-full bg-slate-100 pt-6 pb-48">
             <div className="max-w-6xl w-full  md:px-12 px-4 text-left flex-col ">
-                <h1 className="text-2xl leading-10 font-bold">Start shipping</h1>
-                <p className="my-24 text-xl">
-                    Start designing your SaaS in minutes with the boilerplate and documentation provided.
-                </p>
                 <h2 className="text-2xl leading-10 font-bold">Get the boilerplate</h2>
                 <p className="my-24 text-xl">
-                    Boilerplate with DaisyUI <Link href="https://github.com/nazimboudeffa/boilerplate-rapidosaas-nextjs-mongo-daisyui">https://github.com/nazimboudeffa/boilerplate-rapidosaas-nextjs-mongo-daisyui</Link>
+                    <Link href="https://github.com/nazimboudeffa/boilerplate-rapidosaas-nextjs-mongo-daisyui">https://github.com/nazimboudeffa/boilerplate-rapidosaas-nextjs-mongo-daisyui</Link>
                 </p>
                 <h2 className="text-2xl  leading-10 font-bold">Get the documentation</h2>
                 <p className="my-24 text-xl">
